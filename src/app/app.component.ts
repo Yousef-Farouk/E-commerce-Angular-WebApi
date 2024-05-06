7c6c3b34d212f242bf2d1f62e077136042f9dd63
Yousef-Farouk <yfrouk853@gmail.com>
2024-05-06 14:37:53 +0300

    adding product details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { BannerComponent } from './components/banner/banner.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductDetailsComponent } from './components/products/product-details/product-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SliderComponent,BannerComponent,OverviewComponent,ProductsComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,SliderComponent,BannerComponent,OverviewComponent,ProductsComponent,FooterComponent,ProductDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  search_flag :boolean = false ;
+  filter_flag :boolean = false ;
+
+  displaySearch()
+  {
+      this.search_flag = !this.search_flag;
+  }
+
+  displayFilter()
+  {
+    this.filter_flag = !this.filter_flag
+  }
 }
