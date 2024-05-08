d7d4cad53c20246694db23247938ecf067c6fe53
Yousef-Farouk <yfrouk853@gmail.com>
2024-05-09 00:40:23 +0300

    adding product-information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { ProductsComponent } from './components/products/products.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductDetailsComponent } from './components/products/product-details/product-details.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ProductInformationComponent } from './components/products/product-information/product-information.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SliderComponent,BannerComponent,OverviewComponent,ProductsComponent,FooterComponent,ProductDetailsComponent,ShoppingCartComponent],
+  imports: [RouterOutlet,NavbarComponent,SliderComponent,BannerComponent,OverviewComponent,ProductsComponent,FooterComponent,ProductDetailsComponent,ShoppingCartComponent,ProductInformationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/products/product-information/product-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-information.component.html',
+  styleUrl: './product-information.component.css'
+})
+export class ProductInformationComponent {
+
+}
